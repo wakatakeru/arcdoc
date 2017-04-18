f6d77800827b8d66286a7b2816e22c7275a1501c
wakatakeru <takeru.eng@gmail.com>
2017-04-18 13:04:39 -0400

    ドキュメントモデルにtag_idを追加

--- db/migrate/20170418160250_create_documents.rb
+++ db/migrate/20170418160250_create_documents.rb
@@ -4,7 +4,7 @@ class CreateDocuments < ActiveRecord::Migration[5.0]
       t.string :title
       t.text :content
       t.string :author_id
-
+      t.integer :tag_id
       t.timestamps
     end
   end
