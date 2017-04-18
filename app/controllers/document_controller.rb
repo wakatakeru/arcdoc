a783c2358e3edf09769ad40a581221fdc932ae22
wakatakeru <takeru.eng@gmail.com>
2017-04-18 13:02:03 -0400

    ドキュメントの新規作成を実装

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -1,2 +1,27 @@
 class DocumentController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+  
+  def new
+    @document = Document.new
+  end
+
+  def create
+    document = Document.new
+
+    document.title   = params['document']['title']
+    document.content = params['document']['content']
+    
+    if document.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
 end
