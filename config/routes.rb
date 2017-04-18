7125d121964b7457005795ed92e8e568a3d2e201
wakatakeru <takeru.eng@gmail.com>
2017-04-18 12:25:44 -0400

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'root/index'
+  root to: 'root#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :user
+  resources :document
+  resources :tag
 end
