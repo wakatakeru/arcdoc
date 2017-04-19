8f271afff135df84640d63032cc65e93791725f4
wakatakeru <takeru.eng@gmail.com>
2017-04-18 22:37:38 -0400

    ドキュメントコントローラとビューの最低限の機能を実装

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -1,8 +1,10 @@
 class DocumentController < ApplicationController
   def index
+    @document = Document.new
   end
 
   def show
+    @document = Document.find(params['id'])
   end
   
   def new
@@ -23,5 +25,32 @@ class DocumentController < ApplicationController
   end
 
   def edit
+    @document = Document.find(params['id'])
+  end
+
+  def update
+    @document = Document.find(params['id'])
+    
+    @document.title   = params['document']['title']
+    @document.content = params['document']['content']
+   
+    if @document.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    @document = Document.find(params['id'])
+    
+    if @document.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 end
+
+
+
