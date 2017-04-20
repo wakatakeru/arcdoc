48c167e48f81be5a7feb9b8d399d243fe82d77fa
wakatakeru <takeru.eng@gmail.com>
2017-04-19 22:04:45 -0400

    ユーザモデルを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  has_many :documents, :dependent => :destroy
+  has_secure_password
+
+  validates :login_id,
+            :presence => true,
+            :uniqueness => true,
+            :format => { :with => /\A[a-z0-9]+\z/i }
+
+  validates :password,
+            :presence => true,
+            :confirmation => true,
+            :format => { :with => /\A[a-z0-9]+\z/i },
+            :on => :create
+
+  def validate_password?
+    password.presence? || password_confirmation.presence?
+  end
 end
