eb838b9d47a8bb2d83b0db95c055fc6e3e00482f
wakatakeru <takeru.eng@gmail.com>
2017-04-20 05:00:09 -0400

    ユーザ認証を修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,38 +5,38 @@ class UserController < ApplicationController
   def index
     user = User.find(session[:login_id])
     if user[:is_admin] == false
-      flash[:alert] = "表示する権限がありません"
+      flash[:danger] = "表示する権限がありません"
       redirect_to root_index_path
     end
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:login_id])
+    @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
-    flash[:alert] = "このアカウントには管理者権限が付与されます" if User.all.empty?
+    flash[:warning] = "このアカウントには管理者権限が付与されます" if User.all.empty?
   end
 
   def create
     user = User.new
 
-    user.login_id              = params['user']['login_id']
-    user.secreen_name          = params['user']['screen_name']
-    user.password              = params['user']['password']
-    user.password_confirmation = params['user']['password_confirmation']
+    user.login_id              = params['login_id']
+    user.screen_name           = params['screen_name']
+    user.password              = params['password']
+    user.password_confirmation = params['password_confirmation']
 
     user.is_admin = false
     user.is_admin = true if User.all.empty?
 
     if user.save
       flash[:success] = "ユーザを正常に登録しました"
-      flash[:success] = "ユーザを管理者として正常に登録しました" if user == User.first
+      flash[:info] = "ユーザを管理者として正常に登録しました" if user == User.first
       redirect_to login_path
     else
-      flash[:alert] = "ユーザが登録できませんでした。ユーザ名を変更してください"
+      flash[:danger] = "ユーザが登録できませんでした。ユーザ名を変更してください"
       @user = user
       render 'new'
     end
@@ -44,7 +44,7 @@ class UserController < ApplicationController
 
   def edit
     if session[:login_id].to_i != params[:id].to_i
-      flash[:alert] = "ページの表示権限がありません"
+      flash[:danger] = "ページの表示権限がありません"
       redirect_to root_index_path
     end
     @user = User.find(params[:id].to_i)
@@ -61,7 +61,7 @@ class UserController < ApplicationController
       flash[:success] = "ユーザ情報を正常に変更しました"
       redirect_to root_index_path
     else
-      flash[:alert] = "ユーザが登録できませんでした。パスワードが一致しません"
+      flash[:danger] = "ユーザが登録できませんでした。パスワードが一致しません"
       @user = user
       render 'edit'
     end
@@ -69,12 +69,12 @@ class UserController < ApplicationController
 
   def destroy
     user = User.find(params[:id].to_i)
-    if user.destroy
+    if !User.all.empty? && user.is_admin != true && user.destroy
       flash[:success] = "ユーザを正常に削除しました"
       redirect_to login_path
     else
-      flash[:alert] = "ユーザを削除できません"
-      redirect_to root_index_path
+      flash[:danger] = "ユーザを削除できません"
+      redirect_to user_index_path
     end
   end
 
